009713726cfbf10f0db8cca8fa46a4cea89b32cb
luisagalva <luisabeyu200797@gmail.com>
2018-03-07 20:48:15 -0600

    Initial Commit

---
+++ Consultorio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Consultorio
+//
+//  Created by Luisa Beatriz Garcia Alva on 07/03/18.
+//  Copyright © 2018 Luisa Beatriz Garcia Alva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
