f907bcf93b71d7445ca84d1d1fed77e400066df2
luisagalva <luisabeyu200797@gmail.com>
2018-05-02 22:47:52 -0500

    evidencia 2

---
+++ Consultorio/WeekdaysView.swift
@@ -0,0 +1,9 @@
+//
+//  WeekdaysView.swift
+//  Consultorio
+//
+//  Created by Luisa Beatriz Garcia Alva on 11/03/18.
+//  Copyright © 2018 Luisa Beatriz Garcia Alva. All rights reserved.
+//
+
+import Foundation
